129b6843f1e52debc4f5d2382e206804ca2d4d6b
Nathan <skeks463@gmail.com>
2019-06-25 11:33:28 +0900

    Add Stack

---
+++ Stack/Stack.py
@@ -0,0 +1,45 @@
+from DoublyLinkedList.DoublyLinkedList import Node
+from DoublyLinkedList.DoublyLinkedList import DoublyLinkedList
+
+
+class ArrayStack:
+
+    def __init__(self):
+        self.data = []
+
+    def size(self):
+        return len(self.data)
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def push(self, item):
+        self.data.append(item)
+
+    def pop(self):
+        return self.data.pop()
+
+    def peek(self):
+        return self.data[-1]
+
+
+class LinkedListStack:
+
+    def __init__(self):
+        self.data = DoublyLinkedList()
+
+    def size(self):
+        return self.data.getLength()
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def push(self, item):
+        node = Node(item)
+        self.data.insertAt(self.size() + 1, node)
+
+    def pop(self):
+        return self.data.popAt(self.size())
+
+    def peek(self):
+        return self.data.getAt(self.size()).data
