4129506789148684e48e9ccc174c744e0d4ea169
Nathan <skeks463@gmail.com>
2019-06-25 14:11:23 +0900

    Modify infixTopostfix

--- Stack/infixToPostfix.py
+++ Stack/infixToPostfix.py
@@ -5,7 +5,16 @@ from Stack import ArrayStack as Stack
 from splitTokens import splitTokens
 
 def infixToPostfix(tokenList):
+    '''
+    Algoritms Architecture
 
+    1) 피연산자이면 그냥 출력
+    2) '('이면 스택에 push
+    3) ')' 이면 '('이 나올때 까지 스택에서 pop, 출력
+    4) 연산자이면 스택에서 이보다 높(거나 같)은 우선순위 것들을 pop, 출력
+    그리고 이 연산자는 스택에 push
+    5) 스택에 남아 있는 연산자는 모두 pop, 출력.
+    '''
     prec = {
         '*': 3, '/': 3,
         '+': 2, '-': 2,
