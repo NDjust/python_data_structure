5c1c1b7e0e7d556712ff96f0bb6287b9b1c7c1ad
Nathan <skeks463@gmail.com>
2019-06-26 15:34:19 +0900

    Modify CircularQueue

--- queue/CircularQueue.py
+++ queue/CircularQueue.py
@@ -20,16 +20,18 @@ class CircularQueue:
     def enqueue(self, x):
         if self.isFull():
             raise IndexError('Queue full')
-        self.rear = 0 if self.rear == self.maxCount - 1 else self.rear + 1
-        # self.rear = (self.rear + 1) % (self.maxCount)
+
+        self.rear = (self.rear + 1) % (self.maxCount)
+        # self.rear = 0 if self.rear == self.maxCount - 1 else self.rear + 1
         self.data[self.rear] = x
         self.count += 1
     
     def dequeue(self):
         if self.is_Empty():
             raise IndexError('Queue empty')
-        self.front = 0 if self.front == self.maxCount - 1 else self.front + 1
-        # self.front = (self.front + 1) % (self.maxCount)
+
+        self.front = (self.front + 1) % (self.maxCount)
+        # self.front = 0 if self.front == self.maxCount - 1 else self.front + 1
         x = self.data[self.front]
         self.count -= 1
         return x
@@ -37,5 +39,7 @@ class CircularQueue:
     def peek(self):
         if self.is_Empty():
             raise IndexError('Queue empty')
-        return self.data[0 if self.front == self.maxCount - 1 else self.front + 1]
-        # return self.data[(self.front + 1) % (self.maxCount)]
+        
+        return self.data[(self.front + 1) % (self.maxCount)]
+        # return self.data[0 if self.front == self.maxCount - 1 else self.front + 1]
+        
